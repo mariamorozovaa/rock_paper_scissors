f767b051f0975ef53cb1248a7621610fd589db35
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2024-12-18 12:37:04 +0300

    Add a function to play a full game
    It was necessary to create the game logic so that
    the user can see the results of rounds and
    provisional result and final result of game.
    Added a cycle to play 5 rounds.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice () {
     let randomNum = Math.floor(Math.random() * 100);
     let compChoice;
 
     if ((randomNum >= 0) && (randomNum < 33)) {
         compChoice = 'rock';
-    } 
+    }
     else if ((randomNum >= 33) && (randomNum < 66)) {
         compChoice = 'paper';
-    } 
+    }
     else {
         compChoice = 'scissors';
     }
@@ -24,43 +21,67 @@ function getHumanChoice () {
     return sign;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+    let countOfRounds = 0;
 
-function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    while (countOfRounds < 5) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        
+        countOfRounds += 1;
+        console.log(`${countOfRounds} round behind`);
+    }
 
-    if (humanChoice === computerChoice) {
-        console.log(`It\'s a draw. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+    function playRound (humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice === computerChoice) {
+            console.log(`It\'s a draw. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+        }
+        else if ((humanChoice === 'rock') && (computerChoice === 'paper')) {
+            console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if ((humanChoice === 'paper') && (computerChoice === 'rock')) {
+            console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            humanScore += 1;
+        }
+        else if ((humanChoice === 'paper') && (computerChoice === 'scissors')) {
+            console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if ((humanChoice === 'scissors') && (computerChoice === 'paper')) {
+            console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            humanScore += 1;
+        }
+        else if ((humanChoice === 'scissors') && (computerChoice === 'rock')) {
+            console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if ((humanChoice === 'rock') && (computerChoice === 'scissors')) {
+            console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
+            humanScore += 1;
+        }
+        else {
+            console.log('Please chose correct word');
+        }
+        console.log(`Your score: ${humanScore}. Computer\'s score: ${computerScore}`);
     }
-    else if ((humanChoice === 'rock') && (computerChoice === 'paper')) {
-        console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        computerScore =+ 1;
+
+    if (humanScore > computerScore) {
+        console.log('Congratulatons! You win in this game!!!', `Your score: ${humanScore}. Computer\'s score: ${computerScore}`);
     }
-    else if ((humanChoice === 'paper') && (computerChoice === 'rock')) {
-        console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        humanScore =+ 1;
+    
+    else if (humanScore < computerScore) {
+        console.log('Oh no! You lose in this game :(', `Your score: ${humanScore}. Computer\'s score: ${computerScore}`);
     }
-    else if ((humanChoice === 'paper') && (computerChoice === 'scissors')) {
-        console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        computerScore =+ 1;
-    }
-    else if ((humanChoice === 'scissors') && (computerChoice === 'paper')) {
-        console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        humanScore =+ 1;
-    }
-    else if ((humanChoice === 'scissors') && (computerChoice === 'rock')) {
-        console.log(`You lose. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        computerScore =+ 1;
-    }
-    else if ((humanChoice === 'rock') && (computerChoice === 'scissors')) {
-        console.log(`You win. The computer chose: ${computerChoice}. You chose: ${humanChoice}`);
-        humanScore =+ 1;
-    }
+    
     else {
-        console.log('Please chose correct word');
+        console.log('It\'s a draw in this game. How is it possible?', `Your score: ${humanScore}. Computer\'s score: ${computerScore}`);
     }
 }
 
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score: ${humanScore}. Computer\'s score: ${computerScore}`);
+playGame();
