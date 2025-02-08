af5fde2c9f985ae1e7f541de138fd6acb90edc53
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-02-08 11:46:02 +0300

    Fixed a bug to show the final score
    Add a function to show the final score at the end
    of the game after getting 5 points for one of the players.

--- script.js
+++ script.js
@@ -15,21 +15,18 @@ function getComputerChoice() {
 
 const buttons = document.querySelectorAll("button");
 let choiceOfHuman;
+let humanScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         choiceOfHuman = button.id;
         playRound(choiceOfHuman, getComputerChoice());
+        if (humanScore === 5 || computerScore === 5) {
+            showResults();
+        }
     });
 });
-
-let humanScore = 0;
-let computerScore = 0;
-
-const score = document.querySelector("#score");
-const textOfScore = document.createElement("p");
-textOfScore.setAttribute("style", "font-size: 25px;");
-score.appendChild(textOfScore);
 
 const result = document.querySelector("#result");
 const textOfResults = document.createElement("p");
@@ -37,37 +34,47 @@ textOfResults.setAttribute("style", "font-size: 25px;");
 result.appendChild(textOfResults);
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        computerScore += 1;
-        humanScore += 1;
-        textOfResults.textContent = `It\'s a draw. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}.\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
-    } else if (
-        (humanChoice === "rock" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "rock")
-    ) {
-        computerScore += 1;
-        textOfResults.textContent = `You lose. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
-    } else if (
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "rock" && computerChoice === "scissors")
-    ) {
-        humanScore += 1;
-        textOfResults.textContent = `You win. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
-    }
-    if (humanScore === 5 || computerScore === 5) {
-        if (humanScore > computerScore) {
-            textOfScore.textContent = `Congratulatons! You win in this game!!!`;
-        } else if (humanScore < computerScore) {
-            textOfScore.textContent = `Oh no! You lose in this game :(`;
-        } else {
-            textOfScore.textContent = `It\'s a draw in this game. How is it possible?`;
+    if (humanScore < 5 && computerScore < 5) {
+        if (humanChoice === computerChoice) {
+            computerScore += 1;
+            humanScore += 1;
+            textOfResults.textContent = `It\'s a draw. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}.\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
+        } else if (
+            (humanChoice === "rock" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "rock")
+        ) {
+            computerScore += 1;
+            textOfResults.textContent = `You lose. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
+        } else if (
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "rock" && computerChoice === "scissors")
+        ) {
+            humanScore += 1;
+            textOfResults.textContent = `You win. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
         }
-        // const restart = document.createElement("button");
-        // restart.textContent("Restart");
-        // сделать кнопки неактивными до нажатия кнопки рестарт
-        // после нажатия кнопки рестарт сбросить счет и результат и сделать кнопки активными
-        // переработать интерфейс
     }
 }
+
+const score = document.querySelector("#score");
+const textOfScore = document.createElement("p");
+textOfScore.setAttribute("style", "font-size: 25px;");
+score.appendChild(textOfScore);
+
+function showResults() {
+    if (humanScore > computerScore) {
+        textOfScore.textContent = `Congratulatons! You win in this game!!!`;
+    } else if (humanScore < computerScore) {
+        textOfScore.textContent = `Oh no! You lose in this game :(`;
+    } else {
+        textOfScore.textContent = `It\'s a draw in this game. How is it possible?`;
+    }
+}
+
+// const restart = document.createElement("button");
+// restart.textContent("Restart");
+
+// после нажатия кнопки рестарт сбросить счет и результат и сделать кнопки активными
+// сделать в случае ничьи +0 +0
+// переработать интерфейс
