185f5db82808a60eed55509c37515f1db55e0a93
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-02-12 14:15:07 +0300

    Change the design, fix a bug, add a restart button
    Fixed a bug that prevented the game result from being
    displayed when one player scored 5 points, added a restart
    button that allows you to start the game again.

--- script.js
+++ script.js
@@ -36,45 +36,53 @@ result.appendChild(textOfResults);
 function playRound(humanChoice, computerChoice) {
     if (humanScore < 5 && computerScore < 5) {
         if (humanChoice === computerChoice) {
-            computerScore += 1;
-            humanScore += 1;
-            textOfResults.textContent = `It\'s a draw. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}.\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
+            // computerScore += 1;
+            // humanScore += 1;
+            textOfResults.textContent = `It\'s a draw. Computer also choose ${computerChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
         } else if (
             (humanChoice === "rock" && computerChoice === "paper") ||
             (humanChoice === "paper" && computerChoice === "scissors") ||
             (humanChoice === "scissors" && computerChoice === "rock")
         ) {
             computerScore += 1;
-            textOfResults.textContent = `You lose. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
+            textOfResults.textContent = `You lose. ${computerChoice} beats ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
         } else if (
             (humanChoice === "paper" && computerChoice === "rock") ||
             (humanChoice === "scissors" && computerChoice === "paper") ||
             (humanChoice === "rock" && computerChoice === "scissors")
         ) {
             humanScore += 1;
-            textOfResults.textContent = `You win. \nThe computer chose: ${computerChoice}. You chose: ${humanChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
+            textOfResults.textContent = `You won. ${humanChoice} beats ${computerChoice}\nYour score: ${humanScore}. Computer\'s score: ${computerScore}`;
         }
     }
 }
 
 const score = document.querySelector("#score");
+const headScore = document.createElement("h2");
 const textOfScore = document.createElement("p");
 textOfScore.setAttribute("style", "font-size: 25px;");
+score.appendChild(headScore);
 score.appendChild(textOfScore);
+const restartBtn = document.createElement("button");
+restartBtn.textContent = "Restart";
+restartBtn.classList.add("refresh");
+restartBtn.addEventListener("click", () => {
+    window.location.reload();
+});
 
 function showResults() {
+    headScore.textContent = "Score of Game:";
     if (humanScore > computerScore) {
-        textOfScore.textContent = `Congratulatons! You win in this game!!!`;
+        textOfScore.textContent = `Congratulatons! You won this game!!!`;
     } else if (humanScore < computerScore) {
-        textOfScore.textContent = `Oh no! You lose in this game :(`;
+        textOfScore.textContent = `Oh no! You lose this game :(`;
     } else {
         textOfScore.textContent = `It\'s a draw in this game. How is it possible?`;
     }
+
+    score.appendChild(restartBtn);
 }
 
-// const restart = document.createElement("button");
-// restart.textContent("Restart");
-
-// после нажатия кнопки рестарт сбросить счет и результат и сделать кнопки активными
-// сделать в случае ничьи +0 +0
-// переработать интерфейс
+//сделать например камень бьет ножницы и картинками маленькими
+//сократить отступы
+//изменить кнопку, добавить цветов
